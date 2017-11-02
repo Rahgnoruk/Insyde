866d44618372f6b32d1271ee2eb37087e84e7c09
user132086 <user132086@MAC090.local>
2017-11-02 14:59:32 -0500

    Feed UI Complete

--- Insyde/View/CircleView.swift
+++ Insyde/View/CircleView.swift
@@ -1,23 +1,27 @@
 //
-//  SignInView.swift
+//  CircleView.swift
 //  Insyde
 //
-//  Created by user132086 on 11/1/17.
+//  Created by user132086 on 11/2/17.
 //  Copyright © 2017 TonyfiedProductions. All rights reserved.
 //
 
 import UIKit
 
-class TopView: UIView {
+class CircleView: UIImageView {
+
     override func awakeFromNib() {
-        super.awakeFromNib();
-        
+        super.awakeFromNib()
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0;
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
         
+        layer.cornerRadius = self.frame.width/2;
     }
 
 }
